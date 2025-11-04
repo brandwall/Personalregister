e5b9952d0bbf68155f9dda7fa2f3a9413c7a2010
Erik <erik.siljestrom@iths.se>
2025-11-04 16:40:56 +0100

    Console Application to input employee and salary.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Personalregister
+﻿using Personalregister.UI;
+using Personalregister.Entities;
+
+namespace Personalregister
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+			var list = new List<Employee>();
+
+			while (true)
+            {
+                try
+                {
+                    
+                    IO.OutputEmployees(list);
+
+					var employee = IO.CreateEmployee();
+
+					list.Add(employee);
+                    Console.Clear();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Try again. ");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                }
+            }
+		}
     }
 }
---
+++ UI/IO.cs
@@ -0,0 +1,79 @@
+﻿using Personalregister.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personalregister.UI
+{
+	internal static class IO
+	{
+		public static Employee CreateEmployee()
+		{
+			Console.Write("Please enter your first name: ");
+			string firstName = Console.ReadLine();
+
+			Console.Write("Please enter your last name: ");
+			string lastName = Console.ReadLine();
+
+			Console.Write("Please enter your salary: ");
+			double salary = Convert.ToDouble(Console.ReadLine());
+
+			return new Employee() { FirstName = firstName, LastName = lastName, Salary = salary };
+		}
+
+		public static void OutputEmployees(List<Employee> employees)
+		{
+			if (employees.Count < 1)
+				return;
+
+			// Adjusts the table headers length depending on content length
+			int longestName = FindLongestName(employees);
+			int longestSalary = FindLongestSalary(employees);
+
+			string tableHeaderName = longestName > 10 ? ("---Name---" + new string('-', (FindLongestName(employees)-10))) : "---Name---";
+			string tableHeaderSalary = longestSalary > 10 ? ("--Salary--" + new string('-', (FindLongestName(employees) - 10))) : "--Salary--";
+
+			// Writes the table headers like so: "---Name----|--Salary--"
+			Console.WriteLine(tableHeaderName + "-|-" + tableHeaderSalary);
+
+			// Writes the table rows, ex: "Erik Siljeström   |   28000"
+			foreach(var emp in employees)
+			{
+				Console.Write(longestName > emp.Name.Length ? (emp.Name + new string(' ', (longestName - emp.Name.Length))) : emp.Name);
+				Console.Write(" | ");
+				Console.WriteLine(longestSalary > emp.Salary.ToString().Length ? (emp.Salary + new string(' ', (longestName - emp.Salary.ToString().Length))) : emp.Salary);
+
+			}
+			Console.WriteLine();
+		}
+
+		private static int FindLongestName(List<Employee> employees) 
+		{
+			int length = 0;
+			foreach (var emp in employees)
+			{
+				if (emp.Name.Length > length)
+					length = emp.Name.Length;
+			}
+			return length;
+		}
+
+		private static int FindLongestSalary(List<Employee> employees)
+		{
+			int length = 0;
+			foreach (var emp in employees)
+			{
+				int salaryLength = emp.Salary.ToString().Length;
+
+				if (salaryLength > length)
+					length = salaryLength;
+			}
+			return length;
+		}
+
+
+	}
+}
